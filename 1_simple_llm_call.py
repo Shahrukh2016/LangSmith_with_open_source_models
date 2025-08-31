25eff2922f0b0f624451f01b84d9c847b037df28
Shahrukh Ahmad <shahrukhspn2016@gmail.com>
2025-08-31 13:12:57 +0530

    First commit of LangSmith

--- 1_simple_llm_call.py
+++ 1_simple_llm_call.py
@@ -1,14 +1,16 @@
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
+from langchain_perplexity import ChatPerplexity
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+import os
 
 load_dotenv()
 
 # Simple one-line prompt
 prompt = PromptTemplate.from_template("{question}")
 
-model = ChatOpenAI()
+model = ChatPerplexity(model= 'sonar', api_key= os.getenv("PERPLEXITY_API_KEY"))
+
 parser = StrOutputParser()
 
 # Chain: prompt → model → parser
