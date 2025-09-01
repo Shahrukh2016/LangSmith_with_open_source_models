3cb225adbf1bdf89d6b4864488a594beaf1b189c
Shahrukh Ahmad <shahrukhspn2016@gmail.com>
2025-09-01 23:12:02 +0530

    RAG commit of LangSmith

--- 3_rag_v1.py
+++ 3_rag_v1.py
@@ -1,10 +1,9 @@
-# pip install -U langchain langchain-openai langchain-community faiss-cpu pypdf python-dotenv
-
 import os
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_huggingface import HuggingFaceEndpointEmbeddings, ChatHuggingFace
+from langchain_perplexity import ChatPerplexity
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
@@ -26,7 +25,8 @@ splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
 splits = splitter.split_documents(docs)
 
 # 3) Embed + index
-emb = OpenAIEmbeddings(model="text-embedding-3-small")
+emb = HuggingFaceEndpointEmbeddings(model= 'sentence-transformers/all-MiniLM-L6-v2')
+
 vs = FAISS.from_documents(splits, emb)
 retriever = vs.as_retriever(search_type="similarity", search_kwargs={"k": 4})
 
@@ -37,7 +37,8 @@ prompt = ChatPromptTemplate.from_messages([
 ])
 
 # 5) Chain
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+llm = ChatPerplexity(model= 'sonar', api_key= os.getenv("PERPLEXITY_API_KEY"), temperature=0)
+
 def format_docs(docs): return "\n\n".join(d.page_content for d in docs)
 
 parallel = RunnableParallel({
